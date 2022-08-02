ab0a5120c2b283350d5dac8874e9dbb253a27d19
변재호 <bjho606@naver.com>
2022-08-02 15:16:45 +0900

    route 수정

--- Backend/src/app/User/userRoute.js
+++ Backend/src/app/User/userRoute.js
@@ -7,10 +7,10 @@ module.exports = function(app){
     app.get('/app/testdb', user.getTestDB)
 
     // 1. 유저 생성 (회원가입) API
-    app.post('/app/signup', user.postUsers);
+    app.post('/app/users/signup', user.postUsers);
 
     // 2. 로그인 하기 (JWT 생성) API
-    app.post('/app/login', user.login);
+    app.post('/app/users/login', user.login);
 
     // 회원 정보 수정 API (JWT 검증 및 Validation - 메소드 체이닝 방식으로 jwtMiddleware 사용)
     // app.patch('/app/users/:userId', jwtMiddleware, user.patchUsers)
