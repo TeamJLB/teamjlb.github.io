c1cb13d1ae25330f52b25140200187e4ae0669f7
변재호 <bjho606@naver.com>
2022-09-09 17:57:28 +0900

    [fix] 자잘한 오류 수정

--- Backend/src/app/Meeting/meetingDao.js
+++ Backend/src/app/Meeting/meetingDao.js
@@ -93,7 +93,7 @@ async function updatemeetingInfo(connection, id, nickname) {
 
 
 module.exports = {
-  selectAllMeetingByUserId
+  selectAllMeetingByUserId,
 
   insertmeetingInfo,
   selectmeetingId,
