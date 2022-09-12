1714d82690988b271689179d247184ef1b85fd18
변재호 <bjho606@naver.com>
2022-09-13 02:44:59 +0900

    [add] 회의 관련 API 만드는 중..

--- Backend/src/app/Meeting/meetingDao.js
+++ Backend/src/app/Meeting/meetingDao.js
@@ -1,45 +1,88 @@
 // 회원 id로 전체 회의 리스트 조회
 async function selectAllMeetingByUserId(connection, user_id) {
   const selectAllMeetingByIdQuery = `
-                SELECT * 
-                FROM meeting 
-                WHERE id = ?;
-                `;
+    SELECT DISTINCT M.meeting_id, M.meeting_name, M.createdAt
+    FROM Matching MC INNER JOIN SubMeeting SM on MC.sub_meeting_id = SM.sub_meeting_id INNER JOIN Meeting M on SM.meeting_id = M.meeting_id
+    WHERE MC.user_id = ?;
+  `;
   const [meetingRows] = await connection.query(selectAllMeetingByIdQuery, user_id);
   return meetingRows;
 }
 
-// 유저 생성 (회원가입)
-async function insertmeetingInfo(connection, insertmeetingInfoParams) {
-  const insertmeetingInfoQuery = `
-        INSERT INTO meeting(meeting_name, id, password, email, phone)
-        VALUES (?, ?, ?, ?, ?);
-    `;
-  const insertmeetingInfoRow = await connection.query(
-    insertmeetingInfoQuery,
-    insertmeetingInfoParams
-  );
-
-  return insertmeetingInfoRow;
+// 회원 id의 회의 id로 회의 검색
+async function selectMeetingById(connection, selectMeetingParams) {
+  const searchMeetingByIdQuery = `
+    SELECT DISTINCT M.meeting_id, M.meeting_name, M.createdAt
+    FROM Matching MC INNER JOIN SubMeeting SM on MC.sub_meeting_id = SM.sub_meeting_id INNER JOIN Meeting M on SM.meeting_id = M.meeting_id
+    WHERE MC.user_id = ? AND M.meeting_id = ?;
+  `;
+  const meetingRow = await connection.query(searchMeetingByIdQuery, selectMeetingParams);
+  return meetingRow[0];
 }
 
-// 아이디로 회원 조회
-async function selectmeetingId(connection, id) {
-  const selectmeetingIdQuery = `
+// 새 회의 개설
+async function insertMeetingInfo(connection, meeting_name) {
+  const insertMeetingInfoQuery = `
+        INSERT INTO Meeting (meeting_name)
+        VALUE (?);
+  `;
+  const insertMeetingInfoRow = await connection.query(insertMeetingInfoQuery, meeting_name);
+
+  return insertMeetingInfoRow;
+}
+
+// 새 회의의 첫번째 주제 추가
+async function insertSubMeetingInfo(connection, insertSubMeetingInfoParams) {
+  const insertSubMeetingInfoQuery = `
+        INSERT INTO SubMeeting (meeting_id, topic)
+        VALUE (?, ?);
+  `;
+  const insertSubMeetingInfoRow = await connection.query(insertSubMeetingInfoQuery, insertSubMeetingInfoParams);
+
+  return insertSubMeetingInfoRow;
+}
+
+// 회의 주제와 회원 매칭하기
+async function makeMatching(connection, insertMatchingParams) {
+  const insertMatchingInfoQuery = `
+        INSERT INTO Matching (user_id, sub_meeting_id)
+        VALUE (?, ?);
+  `;
+  const insertMatchingInfoRow = await connection.query(insertMatchingInfoQuery, insertMatchingParams);
+
+  return insertMatchingInfoRow;
+}
+
+// 회의 id로 회의 존재 여부 체크
+async function findMeetingId(connection, meeting_id) {
+  const selectMeetingIdQuery = `
                 SELECT * 
-                FROM meeting 
-                WHERE id = ?;
-                `;
-  const [idRows] = await connection.query(selectmeetingIdQuery, id);
-  return idRows;
+                FROM Meeting 
+                WHERE meeting_id = ?;
+  `;
+  const meetingRow = await connection.query(selectMeetingIdQuery, meeting_id);
+  return meetingRow[0];
 }
+
+// 회의 id로 해당 서브회의 id 검색
+async function selectSubMeetingById(connection, meeting_id) {
+  const searchSubMeetingByIdQuery = `
+    SELECT max(SM.sub_meeting_id) AS latest_sub_meeting_id
+    FROM Meeting MT INNER JOIN SubMeeting SM on MT.meeting_id = SM.meeting_id
+    WHERE MT.meeting_id = ?;
+  `;
+  const subMeetingRow = await connection.query(searchSubMeetingByIdQuery, meeting_id);
+  return subMeetingRow[0];
+}
+
 
 // 패스워드 체크
 async function selectmeetingPassword(connection, selectmeetingPasswordParams) {
   const selectmeetingPasswordQuery = `
         SELECT *
         FROM meeting
-        WHERE id = ? AND password = ?;`;
+        WHERE id = ? AND password = ?;
+  `;
   const selectmeetingPasswordRow = await connection.query(
       selectmeetingPasswordQuery,
       selectmeetingPasswordParams
@@ -94,9 +137,13 @@ async function updatemeetingInfo(connection, id, nickname) {
 
 module.exports = {
   selectAllMeetingByUserId,
+  selectMeetingById,
+  insertMeetingInfo,
+  insertSubMeetingInfo,
+  makeMatching,
+  findMeetingId,
+  selectSubMeetingById,
 
-  insertmeetingInfo,
-  selectmeetingId,
   selectmeetingPassword,
   selectmeetingEmail,
   selectmeeting,
--- Backend/src/app/Meeting/meetingProvider.js
+++ Backend/src/app/Meeting/meetingProvider.js
@@ -15,23 +15,32 @@ exports.retrieveAllMeeting = async function (userIdx) {
   // connection 해제
   connection.release();
 
-  return allMeetingListResult;
+  return response(baseResponse.SUCCESS, allMeetingListResult);
 };
 
-exports.retrievemeeting = async function (meetingId) {
+exports.retrieveMeeting = async function (userIdx, meetingId) {
   const connection = await pool.getConnection(async (conn) => conn);
-  const meetingResult = await meetingDao.selectmeetingId(connection, meetingId);
+  const meetingResult = await meetingDao.selectMeetingById(connection, [userIdx, meetingId]);
 
   connection.release();
 
   if (meetingResult.length > 0)
-    return response(baseResponse.SUCCESS, {'unique': false});
+    return response(baseResponse.SUCCESS, meetingResult);
   else
-    return response(baseResponse.SUCCESS, {'unique': true});
+    return errResponse(baseResponse.MEETING_ID_NOT_EXISTS);
 
   // return meetingResult[0]; // 한 명의 유저 정보만을 불러오므로 배열 타입을 리턴하는 게 아닌 0번 인덱스를 파싱해서 오브젝트 타입 리턴
 };
 
+exports.meetingCheckById = async function (meeting_id) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const idCheckResult = await meetingDao.findMeetingId(connection, meeting_id);
+  connection.release();
+
+  return idCheckResult;
+};
+
+// ---
 exports.retrievemeetingList = async function (email) {
 
   //email을 인자로 받는 경우와 받지 않는 경우를 구분하여 하나의 함수에서 두 가지 기능을 처리함
@@ -53,14 +62,6 @@ exports.retrievemeetingList = async function (email) {
 
     return meetingListResult;
   }
-};
-
-exports.idCheck = async function (id) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const idCheckResult = await meetingDao.selectmeetingId(connection, id);
-  connection.release();
-
-  return idCheckResult;
 };
 
 exports.emailCheck = async function (email) {
