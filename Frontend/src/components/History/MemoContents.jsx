4125bc6f23f7dcae25adc81d8b9329c0b438c46b
Haen1231 <dlgodls1231@naver.com>
2022-11-14 00:17:37 +0900

    [수정] 히스토리 메모 text-overflow handling

--- Frontend/src/components/History/MemoContents.jsx
+++ Frontend/src/components/History/MemoContents.jsx
@@ -3,7 +3,7 @@ import styles from "./MemoContents.module.css";
 
 const MemoContents = (props) => {
   const { item } = props;
-  return <div className={styles.memoContents}>{item.memo_content}</div>;
+  return <div className={styles.memoContents} style={{whiteSpace: 'pre-wrap', overflowWrap: 'break-word'}}>{item.memo_content}</div>;
 };
 
 export default MemoContents;
