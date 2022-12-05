53bf34562b79fb64b3b83c725e1756626fd4a7d4
imhynni <omelas329@gmail.com>
2022-12-05 17:01:58 +0900

    design: 로딩중 수정

--- Frontend/src/components/UI/Loading.jsx
+++ Frontend/src/components/UI/Loading.jsx
@@ -3,10 +3,10 @@ import styles from "./Loading.module.css";
 
 const Loading = () => {
   return (
-    <>
-      <div className={styles.loading}>회의 기록 중...</div>
+    <div className={styles.loading}>
+      <div className={styles.loadingText}>회의 기록 중...</div>
       <img src="img/spinner.gif" />
-    </>
+    </div>
   );
 };
 
